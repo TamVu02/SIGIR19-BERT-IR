e63a01f90133dd4dc6f64b9c8ce06f00908ed742
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2023-03-10 15:53:14 +0700

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -91,7 +91,7 @@ class AdamWeightDecayOptimizer(tf.keras.optimizers.Optimizer):
   """A basic Adam optimizer that includes "correct" L2 weight decay."""
 
   def __init__(self,
-               learning_rate=learning_rate,
+               learning_rate,
                weight_decay_rate=0.0,
                beta_1=0.9,
                beta_2=0.999,
