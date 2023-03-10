1cff00a5edce586eb1e225a0a20408c45d7c5404
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2023-03-10 15:15:57 +0700

    Update run_qe_classifier.py

--- run_qe_classifier.py
+++ run_qe_classifier.py
@@ -987,7 +987,7 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
 
         output_spec = None
         if mode == tf.estimator.ModeKeys.TRAIN:
-
+            print(learning_rate)
             train_op = optimization.create_optimizer(
                 total_loss, learning_rate, num_train_steps, num_warmup_steps, use_tpu)
 
