4dfed55e870ef8cb9817a7038734344278ac0339
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2023-03-16 10:48:46 +0700

    Update run_qe_classifier.py

--- run_qe_classifier.py
+++ run_qe_classifier.py
@@ -1163,7 +1163,7 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
                 warmup_steps_float = tf.cast(warmup_steps_int, tf.float32)
 
                 warmup_percent_done = global_steps_float / warmup_steps_float
-                warmup_learning_rate = init_lr * warmup_percent_done
+                warmup_learning_rate = learning_rate * warmup_percent_done
 
                 is_warmup = tf.cast(global_steps_int < warmup_steps_int, tf.float32)
                 learning_rate = ((1.0 - is_warmup) * learning_rate + is_warmup * warmup_learning_rate)
