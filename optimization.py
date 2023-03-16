426170000ce5561d9aa966fd98819313bc90ba96
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2023-03-16 10:20:50 +0700

    Update optimization.py

--- optimization.py
+++ optimization.py
@@ -58,21 +58,22 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu):
   # It is recommended that you use this optimizer for fine tuning, since this
   # is how the model was trained (note that the Adam m/v variables are NOT
   # loaded from init_checkpoint.)
-  #print('Current learning_rate : ',learning_rate)
-#   optimizer=tf.keras.optimizers.Adam(
-#     learning_rate=learning_rate,
-#     beta_1=0.9,
-#     beta_2=0.999,
-#     epsilon=1e-06,
-#   )
-  print('Current learning rate: ',learning_rate)
-  optimizer = AdamWeightDecayOptimizer(
-      learning_rate=learning_rate,
-      weight_decay_rate=0.01,
-      beta_1=0.9,
-      beta_2=0.999,
-      epsilon=1e-6,
-      exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"])
+  print('Current learning_rate : ',learning_rate)
+  optimizer=tf.keras.optimizers.experimental.AdamW(
+    learning_rate=learning_rate,
+    weight_decay_rate=0.01,
+    beta_1=0.9,
+    beta_2=0.999,
+    epsilon=1e-06,
+  )
+  #print('Current learning rate: ',learning_rate)
+#   optimizer = AdamWeightDecayOptimizer(
+#       learning_rate=learning_rate,
+#       weight_decay_rate=0.01,
+#       beta_1=0.9,
+#       beta_2=0.999,
+#       epsilon=1e-6,
+#       exclude_from_weight_decay=["LayerNorm", "layer_norm", "bias"])
 
   if use_tpu:
     optimizer = tf.compat.v1.tpu.CrossShardOptimizer(optimizer)
@@ -94,77 +95,77 @@ def create_optimizer(loss, init_lr, num_train_steps, num_warmup_steps, use_tpu):
   return train_op
 
 
-class AdamWeightDecayOptimizer(tf.keras.optimizers.Optimizer):
-  """A basic Adam optimizer that includes "correct" L2 weight decay."""
+# class AdamWeightDecayOptimizer(tf.keras.optimizers.Optimizer):
+#   """A basic Adam optimizer that includes "correct" L2 weight decay."""
 
-  def __init__(self,
-               learning_rate,
-               weight_decay_rate=0.0,
-               beta_1=0.9,
-               beta_2=0.999,
-               epsilon=1e-6,
-               exclude_from_weight_decay=None,
-               name="AdamWeightDecayOptimizer"):
-    """Constructs a AdamWeightDecayOptimizer."""
-    super(AdamWeightDecayOptimizer, self).__init__(False, name)
+#   def __init__(self,
+#                learning_rate,
+#                weight_decay_rate=0.0,
+#                beta_1=0.9,
+#                beta_2=0.999,
+#                epsilon=1e-6,
+#                exclude_from_weight_decay=None,
+#                name="AdamWeightDecayOptimizer"):
+#     """Constructs a AdamWeightDecayOptimizer."""
+#     super(AdamWeightDecayOptimizer, self).__init__(False, name)
 
-    self.learning_rate = learning_rate
-    self.weight_decay_rate = weight_decay_rate
-    self.beta_1 = beta_1
-    self.beta_2 = beta_2
-    self.epsilon = epsilon
-    self.exclude_from_weight_decay = exclude_from_weight_decay
+#     self.learning_rate = learning_rate
+#     self.weight_decay_rate = weight_decay_rate
+#     self.beta_1 = beta_1
+#     self.beta_2 = beta_2
+#     self.epsilon = epsilon
+#     self.exclude_from_weight_decay = exclude_from_weight_decay
 
-  def apply_gradients(self, grads_and_vars, global_step=None, name=None):
-    """See base class."""
-    assignments = []
-    for (grad, param) in grads_and_vars:
-      if grad is None or param is None:
-        continue
+#   def apply_gradients(self, grads_and_vars, global_step=None, name=None):
+#     """See base class."""
+#     assignments = []
+#     for (grad, param) in grads_and_vars:
+#       if grad is None or param is None:
+#         continue
 
-      param_name = self._get_variable_name(param.name)
+#       param_name = self._get_variable_name(param.name)
 
-      m = tf.Variable(
-          name=param_name + "/adam_m",
-          shape=param.shape.as_list(),
-          dtype=tf.float32,
-          trainable=False,
-          initial_value=tf.zeros_initializer())
-      v = tf.Variable(
-          name=param_name + "/adam_v",
-          shape=param.shape.as_list(),
-          dtype=tf.float32,
-          trainable=False,
-          initial_value=tf.zeros_initializer())
+#       m = tf.Variable(
+#           name=param_name + "/adam_m",
+#           shape=param.shape.as_list(),
+#           dtype=tf.float32,
+#           trainable=False,
+#           initial_value=tf.zeros_initializer())
+#       v = tf.Variable(
+#           name=param_name + "/adam_v",
+#           shape=param.shape.as_list(),
+#           dtype=tf.float32,
+#           trainable=False,
+#           initial_value=tf.zeros_initializer())
 
-      # Standard Adam update.
-      next_m = (
-          tf.multiply(self.beta_1, m) + tf.multiply(1.0 - self.beta_1, grad))
-      next_v = (
-          tf.multiply(self.beta_2, v) + tf.multiply(1.0 - self.beta_2,
-                                                    tf.square(grad)))
+#       # Standard Adam update.
+#       next_m = (
+#           tf.multiply(self.beta_1, m) + tf.multiply(1.0 - self.beta_1, grad))
+#       next_v = (
+#           tf.multiply(self.beta_2, v) + tf.multiply(1.0 - self.beta_2,
+#                                                     tf.square(grad)))
 
-      update = next_m / (tf.sqrt(next_v) + self.epsilon)
+#       update = next_m / (tf.sqrt(next_v) + self.epsilon)
 
-      # Just adding the square of the weights to the loss function is *not*
-      # the correct way of using L2 regularization/weight decay with Adam,
-      # since that will interact with the m and v parameters in strange ways.
-      #
-      # Instead we want ot decay the weights in a manner that doesn't interact
-      # with the m/v parameters. This is equivalent to adding the square
-      # of the weights to the loss with plain (non-momentum) SGD.
-      if self._do_use_weight_decay(param_name):
-        update += self.weight_decay_rate * param
+#       # Just adding the square of the weights to the loss function is *not*
+#       # the correct way of using L2 regularization/weight decay with Adam,
+#       # since that will interact with the m and v parameters in strange ways.
+#       #
+#       # Instead we want ot decay the weights in a manner that doesn't interact
+#       # with the m/v parameters. This is equivalent to adding the square
+#       # of the weights to the loss with plain (non-momentum) SGD.
+#       if self._do_use_weight_decay(param_name):
+#         update += self.weight_decay_rate * param
 
-      update_with_lr = self.learning_rate * update
+#       update_with_lr = self.learning_rate * update
 
-      next_param = param - update_with_lr
+#       next_param = param - update_with_lr
 
-      assignments.extend(
-          [param.assign(next_param),
-           m.assign(next_m),
-           v.assign(next_v)])
-    return tf.group(*assignments, name=name)
+#       assignments.extend(
+#           [param.assign(next_param),
+#            m.assign(next_m),
+#            v.assign(next_v)])
+#     return tf.group(*assignments, name=name)
 
   def _do_use_weight_decay(self, param_name):
     """Whether to use L2 weight decay for `param_name`."""
