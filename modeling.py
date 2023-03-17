ec8678a4e25b2213b1e6d2aadfe7a8ef46a865e1
TamVu02 <104774591+TamVu02@users.noreply.github.com>
2023-03-17 16:04:44 +0700

    Update modeling.py

--- modeling.py
+++ modeling.py
@@ -379,7 +379,7 @@ def layer_norm_and_dropout(input_tensor, dropout_prob, name=None):
   output_tensor = dropout(output_tensor, dropout_prob)
   return output_tensor
 
-@tf.function
+#@tf.function
 def create_initializer(shape=(),initializer_range=0.02):
   """Creates a `truncated_normal_initializer` with the given range."""
   return tf.random.truncated_normal(shape,stddev=initializer_range,dtype=tf.float32).numpy()
